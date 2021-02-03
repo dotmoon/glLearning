c7d0e3eabf13563e7b47932f1f375a21f2d2fdc0
dotmoon <459393002@qq.com>
2021-02-03 17:58:43 +0800

    mac gl window

--- glLearning/test/main.cpp
+++ glLearning/test/main.cpp
@@ -160,6 +160,10 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
 	auto window = glfwCreateWindow(800, 480, "testGLFW", nullptr, nullptr);
 	if (!window)
 	{
